98beb69c9537197e99b46d1ec17ef23534619410
Jake Bentvelzen <jake@silverstripe.com.au>
2017-04-06 11:48:07 +1000

    feat(formatWithColor): Add ability to use ANSI color functionality from PHPUnit. (ie. highlight text in GREEN or RED when a task succeeds or fails)

--- SilverStripeBuildTask.php
+++ SilverStripeBuildTask.php
@@ -10,7 +10,31 @@ All code covered by the BSD license located at http://silverstripe.org/bsd-licen
  * @author marcus
  */
 abstract class SilverStripeBuildTask extends Task {
-    protected $cleanupEnv = false;
+	/**
+	 * @see formatWithColor()
+	 * @var array
+	 */
+	private static $ansiCodes = array(
+		'bold'       => 1,
+		'fg-black'   => 30,
+		'fg-red'     => 31,
+		'fg-green'   => 32,
+		'fg-yellow'  => 33,
+		'fg-blue'    => 34,
+		'fg-magenta' => 35,
+		'fg-cyan'    => 36,
+		'fg-white'   => 37,
+		'bg-black'   => 40,
+		'bg-red'     => 41,
+		'bg-green'   => 42,
+		'bg-yellow'  => 43,
+		'bg-blue'    => 44,
+		'bg-magenta' => 45,
+		'bg-cyan'    => 46,
+		'bg-white'   => 47
+	);
+
+	protected $cleanupEnv = false;
 	
 	protected function configureEnvFile() {
 		// fake the _ss_environment.php file for the moment
@@ -58,10 +82,10 @@ TEXT;
 	protected function getInput($prompt) {
 		require_once 'phing/input/InputRequest.php';
 		$request = new InputRequest($prompt);
-        $request->setPromptChar(':');
-        
-        $this->project->getInputHandler()->handleInput($request);
-        $value = $request->getInput();
+		$request->setPromptChar(':');
+		
+		$this->project->getInputHandler()->handleInput($request);
+		$value = $request->getInput();
 		return $value;
 	}
 
@@ -79,5 +103,39 @@ TEXT;
 		}
 		
 		return $ret;
+	}
+
+	/**
+	 * Formats a buffer with a specified ANSI color sequence if colors are
+	 * enabled. (Taken from PHPUnit)
+	 *
+	 * eg. 	$this->formatWithColor("fg-white, bg-red", "ERROR")
+	 *		$this->formatWithColor("fg-black, bg-green", "SUCCESS");
+	 *
+	 * @param string $color
+	 * @param string $buffer
+	 *
+	 * @return string
+	 */
+	protected function formatWithColor($color, $buffer)
+	{
+		$codes   = array_map('trim', explode(',', $color));
+		$lines   = explode("\n", $buffer);
+		$padding = max(array_map('strlen', $lines));
+		$styles  = array();
+
+		foreach ($codes as $code) {
+			$styles[] = self::$ansiCodes[$code];
+		}
+
+		$style = sprintf("\x1b[%sm", implode(';', $styles));
+
+		$styledLines = array();
+
+		foreach ($lines as $line) {
+			$styledLines[] = $style . str_pad($line, $padding) . "\x1b[0m";
+		}
+
+		return implode("\n", $styledLines);
 	}
 }
